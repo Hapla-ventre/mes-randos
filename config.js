527ebb0532ca7468dd76be8f3e910fd335878744
Hapla-ventre <thomas.porral@gmail.com>
2026-08-16 18:19:02 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+  apiKey: "AIzaSyAgayUPOrqdbUndNDegdTzgin8I9QNk8iQ",
+  authDomain: "mes-randonnees-314e9.firebaseapp.com",
+  projectId: "mes-randonnees-314e9",
+  storageBucket: "mes-randonnees-314e9.firebasestorage.app",
+  messagingSenderId: "513478282263",
+  appId: "1:513478282263:web:1f079b87e55a99192fe052",
+};
